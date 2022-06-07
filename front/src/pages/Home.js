61516da406e5a7388791184aebb0f4d99a6cb912
Axel <axel.etoile.espoire@gmail.com>
2022-06-07 09:14:04 +0200

    Update front

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -4,13 +4,14 @@ import { useSelector } from "react-redux";
 import { getNotes, postNotes, putNotes, delNotes } from "../store/actions/NotesActions";
 import { store } from "../store";
 
-// import { Message, Input, Button } from 'semantic-ui-react'
-import { Card, Input, Button } from 'antd';
+import { Card, Input, Button, Row, Col, Typography } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 export default function Home() {
     const data = useSelector((state) => state.notes.data);
+    const { Title } = Typography;
     const { Meta } = Card;
+    const FontIcons = "32px"
 
     function addNotes() {
         const input = document.getElementById("notes").value;
@@ -42,34 +43,42 @@ export default function Home() {
 
     return (
         <div>
-            <h2>GET</h2>
+            <Title level={3}>All data get</Title>
+            <Row>
+                <Col span={12} offset={6}>
+                    {data.users && data.users.map((el, index) => {
+                        return (
+                            <div key={index}>
+                                <Card
+                                    style={{ marginTop: 20 }}
+                                    actions={[
+                                        <EditOutlined style={{ fontSize: FontIcons, }}  key="edit" onClick={() => editNotes(el.id)} />,
+                                        <DeleteOutlined style={{ fontSize: FontIcons, }}  key="del" onClick={() => deleteNotes(el.id)} />,
+                                    ]}
+                                >
+                                    <Meta
+                                        title={el.title}
+                                        description={<Input maxLength="16" id={'notes_edit-' + el.id} placeholder='Edit...' />}
+                                    />
+                                </Card>
+                            </div>
+                        )
+                    })}
+                </Col>
+            </Row>
 
-
-
-            {data.users && data.users.map((el, index) => {
-                return (
-                    <div key={index}>
-                        <Card
-                            style={{ width: 300, marginTop: 16 }}
-                            actions={[
-                                <EditOutlined key="edit" onClick={() => editNotes(el.id)}/>,
-                                <DeleteOutlined key="del" onClick={() => deleteNotes(el.id)}/>,
-                            ]}
-                        >
-                            <Meta
-                                title={el.title}
-                                description={<Input maxLength="16" id={'notes_edit-' + el.id} placeholder='Edit...' />}
-                            />
-
-                        </Card>
-                    </div>
-                )
-            })}
-            <h2>POST</h2>
-            <label>Your notes:</label><br />
-            <Input maxLength="16" id="notes" type="text" name="notes_add" placeholder='Add title' />
-            <Button onClick={(e) => addNotes(e)} type="primary">Add</Button>
-
+            <Title 
+                style={{ marginTop: 100 }}
+                level={3}>
+                    Add something
+            </Title>
+            
+            <Row>
+                <Col span={12} offset={6}>
+                    <Input maxLength="16" id="notes" type="text" name="notes_add" placeholder='Add title' />
+                    <Button onClick={(e) => addNotes(e)} type="primary">Add</Button>
+                </Col>
+            </Row>
         </div>
     )
 }
